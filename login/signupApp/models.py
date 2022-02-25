cb9c768da8b30efba030d5b3359d461af571137b
subiksha <subikshaparthasarathy@gmail.com>
2022-02-25 21:32:55 +0530

    final signup changes

--- login/signupApp/models.py
+++ login/signupApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Customer:
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    email = models.EmailField()
+    password = models.CharField(max_length=500)
+
+    def register(self):
+        self.save()
+
+    def isExists(self):
+        if Customer.objects.filter(email=self.email):
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_customer_by_email(email):
+        try:
+           return Customer.objects.get(email=email)
+        except:
+           return False
+
+
+
--- login/signupApp/urls.py
+++ login/signupApp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from .import settings
+from .import views
 
 urlpatterns = [
     path('index.html/', views.signup)
+    path('activate/<uidb64>/<token>', views.activate, name="activate")
     
 ]
