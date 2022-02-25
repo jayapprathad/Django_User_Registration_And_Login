1d6e5b393c72ca33274c5e81bf8b766c2aacfaee
Jayappratha <jayapprathad@gmail.com>
2022-02-25 13:37:45 +0530

    home

--- login/login/urls.py
+++ login/login/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from homeApp import views
 from loginApp import views
-from signupApp import views
+from signupApp import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
