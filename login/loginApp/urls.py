f121e78e546172b5da325a87ed0d41f54bd996e6
Jayappratha <jayapprathad@gmail.com>
2022-02-25 18:29:04 +0530

    login

--- login/loginApp/urls.py
+++ login/loginApp/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('index.html/', views.login)
-    
 ]
--- login/loginApp/views.py
+++ login/loginApp/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from imaplib import _Authenticator
+from pyexpat.errors import messages
+from django.shortcuts import redirect, render
 
 # Create your views here.
+
 def login(request):
-    return render(request,'loginApp/index.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = _Authenticator(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            # messages.success(request, "Logged In Sucessfully!!")
+            return render(request, "authentication/index.html",{"fname":fname})
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return redirect('home')
+    
+    return render(request, "loginApp/index.html")
+
+
+
     
