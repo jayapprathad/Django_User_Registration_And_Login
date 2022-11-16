04793596edf37fe064c37b0bb7ce107cf4ff9c3d
Jayappratha <jayapprathad@gmail.com>
2022-11-16 16:16:26 +0530

    mail error fixed

--- login/login/info.py
+++ login/login/info.py
@@ -1,5 +1,6 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'AZ.django.login@gmail.com'
-EMAIL_HOST_PASSWORD = '1A2B3C4D'
+EMAIL_HOST_USER = '123018044@sastra.ac.in'
+EMAIL_HOST_PASSWORD = 'zskaszsiocrdgops'
 EMAIL_PORT = 587
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
