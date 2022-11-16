094f420a735077b6156ebf269d1016b46c111604
Jayappratha D <66251674+jayapprathad@users.noreply.github.com>
2022-11-16 19:36:30 +0530

    Update info.py

--- login/login/info.py
+++ login/login/info.py
@@ -1,6 +1,6 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '123018044@sastra.ac.in'
-EMAIL_HOST_PASSWORD = 'zskaszsiocrdgops'
+EMAIL_HOST_USER = 'mail'
+EMAIL_HOST_PASSWORD = 'pass'
 EMAIL_PORT = 587
 EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
